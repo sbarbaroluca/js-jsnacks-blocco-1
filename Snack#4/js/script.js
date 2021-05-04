0b5caf1d92792aaebd453f825b89d88852c42c53
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-05-04 17:25:15 +0200

    fine

--- Snack#4/js/script.js
+++ Snack#4/js/script.js
@@ -3,18 +3,18 @@
 var nomi = ["Luca", "Marco", "Luigi", "Sergio", "Loris"];
 var cognomi = ["Sbarbaro", "Piatti", "Pederzani", "Pianarosa", "Bianchi"];
 
-for (var i = 0; i < 10; i++) {
-    invitato = generaCoppia(nomi, cognomi);
+for (var i = 0; i < 10; i ++) {
+    invitato = generaCoppia(nomi,cognomi);
     console.log(invitato);
     document.getElementById("lista").innerHTML += "<li>" + invitato + "</li>"
 }
 
-function generaCoppia(nome, cognome) {
+function generaCoppia(nome,cognome) {
     var persona;
-    persona = nome[randomNumber(0, 4)] + " " + cognome[randomNumber(0, 4)];
+    persona = nome[randomNumber(0,4)] + " " + cognome[randomNumber(0,4)];
     return persona;
 }
 
-function randomNumber(min, max) {
+function randomNumber(min,max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
