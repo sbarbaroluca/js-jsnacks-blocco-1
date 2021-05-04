343a26fefc168225909227fe050b717727ab7f5f
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-05-04 15:49:13 +0200

    inizio

---
+++ Snack#4/js/script.js
@@ -0,0 +1,2 @@
+// Generatore di “nomi cognomi” casuali: scrivere una funzione che, partendo da una lista di nomi e una lista di cognomi, generi una coppia nome-cognome casuale, così da permettere al signor Gatsby di costruire una falsa lista di 10 invitati alla sua festa, da stampare in pagina, utilizzando il tag adatto alle liste.
+
---
+++ Snack#5/js/script.js
@@ -0,0 +1,3 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero, se è dispari (verifichiamolo con una funzione) inseriscilo nell’array.
+// Stampa l'array nella console.
