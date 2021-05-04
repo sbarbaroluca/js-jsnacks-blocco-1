11a37e2a2ef9f1b504abf38c276744494dbfa09e
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-05-04 17:56:26 +0200

    fine entrambi

--- Snack#5/js/script.js
+++ Snack#5/js/script.js
@@ -2,3 +2,18 @@
 // Chiedi per 6 volte all’utente di inserire un numero, se è dispari (verifichiamolo con una funzione) inseriscilo nell’array.
 // Stampa l'array nella console.
 
+function isOdd(numero) {
+  if (numero % 2 != 0) {
+      return true;
+  }
+}
+
+var numeri = [];
+for (var i = 0; i < 6; i ++) {
+  var numero = parseInt(prompt("Inserisci un numero!"));
+  if (isOdd(numero)) {
+      numeri.push(numero);
+  }
+}
+
+console.log(numeri);
